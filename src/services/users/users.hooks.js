9b8aa6472c08fd6d5d2250b9984e9be6d48b8400
Salifu Mutaru <salifumutaru@gmail.com>
2020-02-23 20:15:41 +0000

    try login

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,21 +1,30 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const local = require('@feathersjs/authentication-local');
+const uuidv4 = require('uuid/v4');
 
+const itemId = context => {
+  context.data.userId = uuidv4();
+
+  return context;
+};
 
 module.exports = {
   before: {
     all: [],
     find: [ authenticate('jwt') ],
     get: [ authenticate('jwt') ],
-    create: [  ],
+    create: [ async (context) => {
+      itemId(context);
+      local.hooks.hashPassword({ passwordField: 'password' });
+    }],
     update: [  authenticate('jwt') ],
     patch: [  authenticate('jwt') ],
     remove: [ authenticate('jwt') ]
   },
 
   after: {
-    all: [ 
-    ],
+    all: [],
     find: [],
     get: [],
     create: [],
